00ecec1ef24077bdf3b0333730e5a8d4bf29744c
LNash4 <logan.nash4@t-mobile.com>
2018-08-28 16:06:34 -0700

    created message

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        from: 'bob',
-        text: 'wya?'
-    })
 });
 
 socket.on('newMessage', function (message) {
