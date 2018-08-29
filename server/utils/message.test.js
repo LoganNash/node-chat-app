8f48cf5e93661305a1fccadf2f1f67c0f8cdaf67
LNash4 <logan.nash4@t-mobile.com>
2018-08-29 15:29:12 -0700

    geolocation messeges

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,7 @@
 var expect = require('expect');
 const _ = require('lodash');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate correct message object', () => {
@@ -13,3 +13,16 @@ describe('generateMessage', () => {
         expect(_.pick(message, ['from', 'text'])).toEqual({from, text});
     });
 });
+
+describe('generateLocationMessage', () => {
+    it('should generate correct location object', () => {
+        var from = 'logs';
+        var lat = 1;
+        var lng = -1;
+        var message = generateLocationMessage(from, lat, lng);
+
+        expect(typeof message.createdAt).toBe(typeof new Date().getTime());
+        expect(message.from).toEqual(from);
+        expect(message.url).toEqual(`https://wwww.google.com/maps?q=${lat},${lng}`);
+    })
+})
